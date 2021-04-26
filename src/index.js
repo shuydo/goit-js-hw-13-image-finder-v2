7a35248aaea68a83eac78bafe3728b3a9c25cda2
Shuydo <shuydo12@gmail.com>
2021-04-26 05:41:04 +0300

    Добавлены bootstrap, просмотр в модалке по клику, листание кнопками клавы и экрана, закрытие, lazy-?

--- src/index.js
+++ src/index.js
@@ -1,96 +1,160 @@
 // console.clear();
 import './styles.css';
 const _ = require('lodash');
+import 'bootstrap/dist/css/bootstrap-grid.min.css';
 import API from './apiService';
 import cardTemplate from './card_templ.hbs';
+// import { indexOf } from 'core-js/fn/array';
 
 const inputRef = document.querySelector('.search-form');
 const galRef = document.querySelector('.gallery');
 const moreBtnRef = document.querySelector('.more');
-let searchQuery = '';
+const backwardBtn = document.querySelector('button[data-action="previous"]');
+const forwardBtn = document.querySelector('button[data-action="next"]');
+
+let galImgsRef = [];
+let galUrls = [];
+let searchQuery = 'cat'; // убрать после отладки
 let pageN = 1;
-
+let currNumbChoice = 0;
 // inputRef.addEventListener('input', _.debounce(onInputChange, 500));
-
 inputRef.addEventListener('submit', onSearch);
 moreBtnRef.addEventListener('click', onClickMoreBtn);
 
-function onClickMoreBtn() {
-  // console.log('onClickMoreBtn');
+galRef.addEventListener('click', onGalContainerClick);
+modal.addEventListener('click', click => {
+  console.log('modal click');
+  if (click.currentTarget === click.target) closeModal();
+});
 
-  API.fetchPix(searchQuery, pageN).then(res => {
-    pageN += 1;
-    renderCard(res.hits);
+backwardBtn.addEventListener("click", backward);
+forwardBtn.addEventListener("click", forward);
 
-    // console.log('до скрола');
-      // window.scrollTo(0, 100);
-      // window.scrollTo({ top: 1500, behavior: 'smooth' });
-      // window.scrollTo({top: 2000,left: 100, behavior: 'smooth' });
-      setTimeout(function () {
-        window.scrollTo({top: 3000, behavior: 'smooth' });
-      // window.scrollByPages(1);
+function onGalContainerClick(click) {
+  // click.preventDefault();
+  window.addEventListener('keydown', onKeyPress);
 
-    },2);
-      // auto smooth instant
+  if (click.target.classList.contains('galImg')) {
+    bigImg.src = click.target.dataset.source;
 
-      // window.scrollByPages(1);
-      // window.scrollBy(0, window.innerHeight);
-    // console.log('после скрола');
+    modal.classList.toggle('is-open');
 
-    return;
-  });
+    currNumbChoice = galUrls.map(arr => arr.large).indexOf(bigImg.src);
+  }
 }
 
-// let inputCbInvocationCounter = 0;
+function onKeyPress(evt) {
+  if (evt.code === 'Escape') closeModal();
 
-function onInputChange(event) {
-  // event.preventDefault();
-  // inputCbInvocationCounter += 1;
-  console.log(event.target.value);
-  // if (event.target.value)
-  //   API.fetchCountries(event.target.value)
-  //     .then(res => {
-  //       console.table(`${res.length} - matches`);
-  //       if (res.length > 10) return notify();
+  if (evt.code === 'ArrowRight') forward();
+  if (evt.code === 'ArrowUp') forward();
+  if (evt.code === 'PageDown') forward();
+  if (evt.code === 'Space') forward();
 
-  //       if (res.length > 1) return renderCountrieTen(res);
-  //       return renderCountrie(res[0]);
-  //     })
-  //     .catch(onFetchError);
+  if (evt.code === 'ArrowLeft') backward();
+  if (evt.code === 'ArrowDown') backward();
+  if (evt.code === 'PageUp') backward();
+  if (evt.code === 'Backspace') backward();
 }
-
-function onSearch(evt) {
-  evt.preventDefault();
-  const submitText = evt.currentTarget;
+function forward() {
+  currNumbChoice += 1;
+  if (currNumbChoice === galUrls.length) currNumbChoice = 0;
+  changeImg();
+}
+function backward() {
+  if (currNumbChoice === 0) currNumbChoice = galUrls.length;
+  currNumbChoice -= 1;
+  changeImg();
+}
+function changeImg() {
+  bigImg.src = galUrls[currNumbChoice].large;
+  // contentModal.alt = collection[currNumbChoice].original;
+}
+function closeModal() {
+  modal.classList.toggle('is-open');
+  window.removeEventListener('keydown', onKeyPress);
+  bigImg.src = '';
+  // contentModal.alt = "";
+}
+// function onSearch(evt) {                 // toggle после отладки
+function onSearch() {
+  // evt.preventDefault();                  // toggle после отладки
+  // const submitText = evt.currentTarget;  // toggle после отладки
   // console.log('submitText:_', submitText.elements.query.value, '_searchQuery:_', searchQuery,'_');
-  if (submitText.elements.query.value === searchQuery) return;
+  // if (submitText.elements.query.value === searchQuery) return;// toggle после отладки
   pageN = 1;
   galRef.innerHTML = '';
-  searchQuery = submitText.elements.query.value;
-
-  // console.log('?', searchQuery);
-
+  // searchQuery = submitText.elements.query.value; // toggle после отладки
+  // console.log('?', searchQuery, pageN);
   API.fetchPix(searchQuery, pageN).then(res => {
     // console.log(res.hits[0]);
     pageN += 1;
+
+    buildDB(res.hits);
     return renderCard(res.hits);
   });
-
   //   .catch(onFetchError)
   //   .finally(() => form.reset());
 
   //   galRef.innerHTML = menu.reduce((acc, Val) => acc + mTemplate(Val), '');
 }
-function renderCard(array) {
-  // console.log('arr',array.reduce((acc, Val) => acc + cardTemplate(Val), ''));
-  // galRef.innerHTML = cardTemplate(obj);
 
-  // galRef.innerHTML = array.reduce((acc, Val) => acc + cardTemplate(Val), '');
+function buildDB(arr) {
+  // console.log('build', arr);
+  // arr.forEach(el =>
+  //   // console.log({web:el.webformatURL,large:el.largeImageURL});
+  //   galUrls.push({web:el.webformatURL,large:el.largeImageURL})
+  // );
+  for (let i = 0; i < arr.length; i++) {
+    galUrls.push({ web: arr[i].webformatURL, large: arr[i].largeImageURL });
+  }
+  // console.log(galUrls);
+
+  // console.log([{name:'Mango',surname:'Rudy'}]);
+  // console.log(galUrls);
+}
+
+function onClickMoreBtn() {
+  API.fetchPix(searchQuery, pageN).then(res => {
+    pageN += 1;
+    renderCard(res.hits);
+
+    window.scrollTo({ top: 2000, left: 100, behavior: 'smooth' });
+    setTimeout(function () {
+      window.scrollTo({ top: 3000, behavior: 'smooth' });
+    }, 2);
+
+    buildDB(res.hits);
+    return;
+  });
+}
+
+function onInputChange(event) {
+  console.log(event.target.value);
+}
+
+// function onSearch(evt) {
+//   evt.preventDefault();
+//   const submitText = evt.currentTarget;
+//   if (submitText.elements.query.value === searchQuery) return;
+//   pageN = 1;
+//   galRef.innerHTML = '';
+//   searchQuery = submitText.elements.query.value;
+
+//   API.fetchPix(searchQuery, pageN).then(res => {
+//     pageN += 1;
+//     return renderCard(res.hits);
+//   });
+// }
+function renderCard(array) {
   galRef.insertAdjacentHTML(
     'beforeend',
     array.reduce((acc, Val) => acc + cardTemplate(Val), ''),
   );
-  // galRef.insertAdjacentHTML("beforeend", cardTemplate(array));
-
-  // gridRef.innerHTML = menu.reduce((acc, Val) => acc + mTemplate(Val), '');
 }
+
+window.addEventListener('beforeunload', evt => {
+  evt.preventDefault();
+  evt.returnValue = ' ';
+});
+onSearch(); // toggle 4 отладки
---
+++ src/Уже_imp.js
@@ -0,0 +1,150 @@
+import './styles.css';
+const _ = require('lodash');
+// import 'bootstrap/dist/css/bootstrap-grid.min.css';
+import API from './apiService';
+import cardTemplate from './card_templ.hbs';
+
+const inputRef = document.querySelector('.search-form');
+const galRef = document.querySelector('.gallery');
+const moreBtnRef = document.querySelector('.more');
+
+let galImgsRef = [];
+let galUrls = [];
+let searchQuery = 'cat'; // убрать после отладки
+let pageN = 1;
+let currNumbChoice = 0;
+
+// inputRef.addEventListener('input', _.debounce(onInputChange, 500));
+inputRef.addEventListener('submit', onSearch);
+moreBtnRef.addEventListener('click', onClickMoreBtn);
+
+galRef.addEventListener('click', onGalContainerClick);
+modal.addEventListener('click', click => {
+  if (click.currentTarget === click.target) closeModal();
+});
+
+function onGalContainerClick(click) {
+  // click.preventDefault();
+  window.addEventListener('keydown', onKeyPress);
+
+  if (click.target.classList.contains('galImg')) {
+    bigImg.src = click.target.dataset.source;
+
+    modal.classList.toggle('is-open');
+  }
+}
+
+function onKeyPress(evt) {
+  if (evt.code === 'Escape') closeModal();
+  if (evt.code === 'ArrowRight') forward();
+  if (evt.code === 'ArrowLeft') backward();
+}
+// function forward() {
+//   currNumbChoice += 1;
+//   if (currNumbChoice === collAlts.length) currNumbChoice = 0;
+//   changeImg();
+// }
+// function backward() {
+//   if (currNumbChoice === 0) currNumbChoice = collAlts.length;
+//   currNumbChoice -= 1;
+//   changeImg();
+// }
+// function changeImg() {
+//   contentModal.src = collection[currNumbChoice].original;
+//   contentModal.alt = collection[currNumbChoice].original;
+// }
+function closeModal() {
+  modal.classList.toggle('is-open');
+  window.removeEventListener('keydown', onKeyPress);
+  bigImg.src = '';
+  // contentModal.alt = "";
+}
+// function onSearch(evt) {                 // toggle после отладки
+function onSearch() {
+  // evt.preventDefault();                  // toggle после отладки
+  // const submitText = evt.currentTarget;  // toggle после отладки
+  // console.log('submitText:_', submitText.elements.query.value, '_searchQuery:_', searchQuery,'_');
+  // if (submitText.elements.query.value === searchQuery) return;// toggle после отладки
+  pageN = 1;
+  galRef.innerHTML = '';
+  // searchQuery = submitText.elements.query.value; // toggle после отладки
+
+  // console.log('?', searchQuery);
+
+  API.fetchPix(searchQuery, pageN).then(res => {
+    // console.log(res.hits[0]);
+    pageN += 1;
+
+    buildDB(res.hits);
+    return renderCard(res.hits);
+  });
+  //   .catch(onFetchError)
+  //   .finally(() => form.reset());
+
+  //   galRef.innerHTML = menu.reduce((acc, Val) => acc + mTemplate(Val), '');
+}
+function buildDB(ar) {
+    console.log('build',ar);
+    // arr.forEach(el => 
+    //   // console.log({web:el.webformatURL,large:el.largeImageURL});
+    //   galUrls.push({web:el.webformatURL,large:el.largeImageURL})
+    // );
+    // for (let i = 0; i < arr.length; i++) {
+      // const element = {web:arr[i].webformatURL,large:arr[i].largeImageURL};
+      // galUrls.push()
+    
+  
+    // console.log([{name:'Mango',surname:'Rudy'}]);
+    // console.log(galUrls);
+  }
+  function onClickMoreBtn() {
+    // console.log('onClickMoreBtn');
+  
+    API.fetchPix(searchQuery, pageN).then(res => {
+      pageN += 1;
+      // buildDB(res.hits);
+      renderCard(res.hits);
+  
+      // window.scrollTo({ top: 3000, behavior: 'smooth' });
+  
+      return;
+    });
+  }
+  
+  function onInputChange(event) {
+    // event.preventDefault();
+    // inputCbInvocationCounter += 1;
+    console.log(event.target.value);
+    // if (event.target.value)
+    //   API.fetchCountries(event.target.value)
+    //     .then(res => {
+    //       console.table(`${res.length} - matches`);
+    //       if (res.length > 10) return notify();
+  
+    //       if (res.length > 1) return renderCountrieTen(res);
+    //       return renderCountrie(res[0]);
+    //     })
+    //     .catch(onFetchError);
+  }
+  
+  
+  
+  
+  function renderCard(array) {
+    // console.log('arr',array.reduce((acc, Val) => acc + cardTemplate(Val), ''));
+    // galRef.innerHTML = cardTemplate(obj);
+  
+    // galRef.innerHTML = array.reduce((acc, Val) => acc + cardTemplate(Val), '');
+    galRef.insertAdjacentHTML(
+      'beforeend',
+      array.reduce((acc, Val) => acc + cardTemplate(Val), ''),
+    );
+    // galRef.insertAdjacentHTML("beforeend", cardTemplate(array));
+  
+    // gridRef.innerHTML = menu.reduce((acc, Val) => acc + mTemplate(Val), '');
+  
+    // galImgsRef=document.querySelectorAll('.galImg');
+    // console.log(galImgsRef.length);
+    // console.log(document.querySelector('.galImg'));
+  }
+onSearch(); // toggle 4 отладки
